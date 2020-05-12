bfa22ca501ef09f9b6bcbc061f748d52721ac2df
Javier Umana <javierumanafs@gmail.com>
2020-05-11 22:00:21 -0400

    mvp

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const db = require("../data/dbConfig.js");
 
+const { isValidPost } = require("./accounts-service.js");
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -12,6 +14,64 @@ router.get('/', (req, res) => {
   })
   .catch(err => {
     res.status(500).json({ error: err.message })
+  })
+})
+
+router.get('/:id', (req, res) => {
+  db("accounts")
+  .where({ id: req.params.id })
+  .then(account => {
+    if(account){
+      res.status(200).json({ data: account})
+    } else {
+      res.status(404).json({ message: "No account by that ID"})
+    }
+  })
+  .catch(err =>{
+    res.status(500).json({ error: err.message })
+  })
+})
+
+router.post('/', (req, res) => {
+  const post = req.body;
+
+  if(isValidPost(post)){
+    db("accounts")
+    .insert(post, "id" );
+  }
+});
+
+router.put('/:id', (req, res) =>{ 
+  const newInfo = req.body;
+
+  db("accounts")
+  .where({ id: req.params.id })
+  .update(newInfo)
+  .then(count =>{
+    if(count > 0){
+      res.status(200).json({ data: count })
+    } else {
+      res.status(404).json({ message: "Record not found by that ID"});
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: err.message })
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  db("accounts")
+  .where({ id: req.params.id })
+  .del()
+  .then(count => {
+    if( count > 0){
+      res.status(200).json({ data: count })
+    } else {
+      res.status(404).json({ message: "Record not found by that ID "})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: err.message })
   })
 })
 
